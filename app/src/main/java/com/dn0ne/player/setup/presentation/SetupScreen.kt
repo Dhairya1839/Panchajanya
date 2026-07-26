358fceaca0fac744bb77375c83f3720124ffae13
Dhairya <dhairyabhanushali3535@gmail.com>
2026-07-26 14:59:54 +0530

    Update SetupScreen.kt

--- app/src/main/java/com/dn0ne/player/setup/presentation/SetupScreen.kt
+++ app/src/main/java/com/dn0ne/player/setup/presentation/SetupScreen.kt
@@ -1,6 +1,5 @@
 package com.dn0ne.player.setup.presentation
 
-import com.dn0ne.player.app.presentation.components.SmoothAnimatedText
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
